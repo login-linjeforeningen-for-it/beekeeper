0286c4a9d87defb8aeb5f697c5ce9248a5b80a9d
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-14 16:09:33 +0200

    Improved logs for prod debugging

--- api/src/plugins/ws.ts
+++ api/src/plugins/ws.ts
@@ -14,7 +14,7 @@ export default fp(async function wsPlugin(fastify: FastifyInstance) {
         fastify.clients = beeswarm.get(id)!.size
 
         connection.on('message', (message) => {
-            console.log('CONNECTION INTERACTION', connection, message)
+            console.log('CONNECTION INTERACTION')
             handleMessage(id, connection, message)
         })
 
--- api/src/utils/ws/handleMessage.ts
+++ api/src/utils/ws/handleMessage.ts
@@ -37,8 +37,7 @@ export async function handleMessage(
 ) {
     try {
         const msg = JSON.parse(rawMessage.toString()) as { type?: string, client?: GPT_Client }
-        console.log('RECEIVED MESSAGE IN HANDLEMESSAGE')
-
+        console.log('HANDLING MESSAGE', msg.type)
         switch (msg.type) {
             case 'update':
                 if (!msg.client) {
