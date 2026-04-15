cc7679f486fedbc6fea1266c988574a5cde0e7d7
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-15 14:56:47 +0200

    Added missing refresh

--- api/src/fp.ts
+++ api/src/fp.ts
@@ -3,12 +3,14 @@ import fp from 'fastify-plugin'
 import refreshInternalDashboard from './fp/refreshInternalDashboard.ts'
 import refreshMonitoring from './fp/refreshStatus.ts'
 import refreshDomains from './fp/refreshDomains.ts'
+import refreshMetrics from './fp/refreshMetrics.ts'
 
 export default fp(async (fastify) => {
     async function refresh() {
         refreshInternalDashboard(fastify)
         refreshMonitoring(fastify)
         refreshDomains(fastify)
+        refreshMetrics(fastify)
         fastify.log.info('Queries refreshed')
     }
 
